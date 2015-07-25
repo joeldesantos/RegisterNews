ce1fae3c049a7fe22119b09398a1deb25409034d
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-07-25 10:04:53 -0500

    Video added

--- CM-RegisterNews/Start.m
+++ CM-RegisterNews/Start.m
@@ -16,12 +16,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self initController];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)initController {
+    [self playVideo];
+}
+
+/**********************************************************************************************/
+#pragma mark - Video method
+/**********************************************************************************************/
+-(void)playVideo {
+    //Play video
+    NSString *videoPath = [[NSBundle mainBundle] pathForResource:@"people" ofType:@"mp4"];
+    NSURL *videoUrl     = [NSURL fileURLWithPath:videoPath];
+    
+    self.moviePlayer    =[[MPMoviePlayerController alloc] initWithContentURL:videoUrl];
+    [self.moviePlayer.view setFrame:CGRectMake(0, 0, 1136, 640)];
+    
+    [self.moviePlayer prepareToPlay];
+    [self.moviePlayer setShouldAutoplay:YES];
+    [self.moviePlayer setRepeatMode:MPMovieRepeatModeOne];
+    self.moviePlayer.controlStyle = MPMovieControlStyleNone;
+    [self.vVideo addSubview:self.moviePlayer.view];
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification object:[UIApplication sharedApplication] queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
+        [self.moviePlayer play];
+    }];
+}
+
 
 @end
