1d9dba559f1587987cb1e7e10fe2979009314f73
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-07-25 09:31:42 -0500

    Initial Commit

---
+++ CM-RegisterNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CM-RegisterNews
+//
+//  Created by Walter Gonzalez Domenzain on 25/07/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CM-RegisterNewsTests/CM_RegisterNewsTests.m
@@ -0,0 +1,40 @@
+//
+//  CM_RegisterNewsTests.m
+//  CM-RegisterNewsTests
+//
+//  Created by Walter Gonzalez Domenzain on 25/07/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CM_RegisterNewsTests : XCTestCase
+
+@end
+
+@implementation CM_RegisterNewsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
